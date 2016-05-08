23c51325f4fd93dfe392199ca72a4a408dbdfbbc
Matt Leach <matt.leach25@gmail.com>
2016-05-07 21:36:56 -0700

    More Blog

--- project/static/strava.js
+++ project/static/strava.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 function plot_map(mapid, latlng) {
-  var similar1 = L.map(mapid).setView(latlng[100], 12);
+  var similar1 = L.map(mapid, {scrollWheelZoom: false}).setView(latlng[100], 12);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoibWF0dC1sZWFjaCIsImEiOiJjaW54djNnb2IxN2pqdTJtM3ViOTJjOXU4In0.f2WCG6PNDQcnbJhTcdSEOA'
+      accessToken: 'pk.eyJ1IjoibWF0dC1sZWFjaCIsImEiOiJjaW54djNnb2IxN2pqdTJtM3ViOTJjOXU4In0.f2WCG6PNDQcnbJhTcdSEOA',
   }).addTo(similar1);
 
   var polyline = L.polyline(latlng, {color: 'red'}).addTo(similar1);
